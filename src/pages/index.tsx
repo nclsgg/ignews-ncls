19912c02b6e6c819afc8d54178daa49fed9b9fdf
nclsgg <nicolasguadagno@gmail.com>
2021-09-12 13:15:29 -0300

    Configuring project basis

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import styles from "../styles/home.module.scss"
+import Head from "next/head"
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Hello World</h1>
-    </div>
+    <>
+      <Head>
+        <title> Início | ig.news </title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
   )
 }
