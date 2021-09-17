2d0a80fb7462e158c077d673f22eef169ab06faf
nclsgg <nicolasguadagno@gmail.com>
2021-09-17 13:25:07 -0300

    Header Componentization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link rel="preconnect" href="https://fonts.gstatic.com"></link>
           <link
